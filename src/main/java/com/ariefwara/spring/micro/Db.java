8a9789831c871f6f92f7ca16508467a4f0f3ed02
Arief Warazuhudien <arief.wara@gmail.com>
2023-01-15 23:19:16 +0700

    use std method to express operation

--- src/main/java/com/ariefwara/spring/micro/Db.java
+++ src/main/java/com/ariefwara/spring/micro/Db.java
@@ -3,21 +3,29 @@ package com.ariefwara.spring.micro;
 import org.springframework.stereotype.Component;
 
 import com.ariefwara.spring.micro.db.Block;
-import com.ariefwara.spring.micro.db.basic.Operation;
-import com.ariefwara.spring.micro.db.basic.operation.process.Target;
+import com.ariefwara.spring.micro.db.Op;
 import com.ariefwara.spring.micro.db.basic.operation.process.Affected;
+import com.ariefwara.spring.micro.db.basic.operation.process.Target;
 import com.ariefwara.spring.micro.db.basic.select.Condition;
 import com.ariefwara.spring.micro.db.basic.select.Selected;
 
 @Component
 public class Db {
-
-	public <T> Affected<T> execute(Operation operation, Target<T> on) {
-		return operation.exec(on);
+	
+	public <T> Affected<T> insert(Target<T> on) {
+		return Op.INSERT.exec(on);
+	}
+	
+	public <T> Affected<T> update(Target<T> on) {
+		return Op.UPDATE.exec(on);
+	}
+	
+	public <T> Affected<T> delete(Target<T> on) {
+		return Op.DELETE.exec(on);
 	}
 	
 	public <T> Affected<T> select(Target<T> on) {
-		return null;
+		return Op.SELECT.exec(on);
 	}
 	
 	public <T> Selected<T> select(Class<T> from, Condition where) {
--- src/main/java/com/ariefwara/spring/micro/db/Op.java
+++ src/main/java/com/ariefwara/spring/micro/db/Op.java
@@ -2,10 +2,12 @@ package com.ariefwara.spring.micro.db;
 
 import com.ariefwara.spring.micro.db.basic.operation.Delete;
 import com.ariefwara.spring.micro.db.basic.operation.Insert;
+import com.ariefwara.spring.micro.db.basic.operation.Select;
 import com.ariefwara.spring.micro.db.basic.operation.Update;
 
 public class Op {
 	
+	public static Select SELECT = new Select();
 	public static Insert INSERT = new Insert();
 	public static Update UPDATE = new Update();
 	public static Delete DELETE = new Delete();
