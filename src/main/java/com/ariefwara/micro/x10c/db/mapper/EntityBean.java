ff89fac91f1523e9534cbef3555203906387fed4
Arief Warazuhudien <arief.wara@gmail.com>
2023-01-25 08:24:13 +0700

    create dedicated class

---
+++ src/main/java/com/ariefwara/micro/x10c/db/mapper/EntityBean.java
@@ -0,0 +1,37 @@
+package com.ariefwara.micro.x10c.db.mapper;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ariefwara.micro.x10c.db.flag.Column;
+import com.ariefwara.micro.x10c.db.flag.Entity;
+
+public class EntityBean {
+
+	static Map<Class<?>, Map<String, String>> fieldMaps = new HashMap<>();
+
+	public Map<String, String> fieldMapping(Class<?> type) {
+
+		if (fieldMaps.containsKey(type))
+			return fieldMaps.get(type);
+
+		if (!type.isAnnotationPresent(Entity.class))
+			throw new UndeclaredThrowableException(new Exception("Non Entity Object"));
+		Field[] fields = type.getDeclaredFields();
+
+		Map<String, String> fieldMap = new HashMap<>();
+		for (Field field : fields) {
+			if (!field.isAnnotationPresent(Column.class))
+				continue;
+			fieldMap.put(field.getAnnotation(Column.class).value(), field.getName());
+			field.setAccessible(true);
+		}
+
+		fieldMaps.put(type, fieldMap);
+
+		return fieldMap;
+	}
+	
+}
